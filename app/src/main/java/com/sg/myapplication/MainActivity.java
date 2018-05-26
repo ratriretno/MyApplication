1feb44f48fba954bd771509b61e84ed78c238724
Ratri Project <ratri.retnowati@gmail.com>
2018-05-26 14:31:11 +0700

    adapter main

--- app/src/main/java/com/sg/myapplication/MainActivity.java
+++ app/src/main/java/com/sg/myapplication/MainActivity.java
@@ -2,11 +2,14 @@ package com.sg.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.sg.myapplication.adapter.ProdukAdapter;
 
+public class MainActivity extends AppCompatActivity {
+ProdukAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycleView);
+        adapter = new ProdukAdapter();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+
+
 
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
